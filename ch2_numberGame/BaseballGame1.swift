d83ab9897a03d113ecdc82e57dd709b9d6312792
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-03-14 09:31:08 +0900

    lv.1 clear

---
+++ ch2_numberGame/BaseballGame1.swift
@@ -0,0 +1,41 @@
+//
+//  BaseballGame1.swift
+//  ch2_numberGame
+//
+//  Created by 한철희 on 3/13/24.
+//
+
+import Foundation
+
+// BaseballGame.swift 파일 생성
+class BaseballGame{
+    func start() {
+        let answer = makeAnswer() // 정답을 만드는 함수
+        
+        
+        
+        while true {
+            let input = Int(readLine()!) // 사용자 입력받기
+            
+            if input == answer {
+                print("정답입니다")
+                break
+            } else {
+                print("오답입니다. 다시 해보세요")
+            }
+        }
+    }
+    
+    func makeAnswer() -> Int {
+        // 함수 내부를 구현하기
+        var randomNumber: Int
+        repeat {
+            let hundreds = Int.random(in: 1..<10) * 100
+            let tens = Int.random(in: 0..<10) * 10
+            let units = Int.random(in: 0..<10)
+            randomNumber = hundreds + tens + units
+        } while Set(String(randomNumber)).count < 3 // 중복된 숫자가 있는 경우 다시 생성
+        print(randomNumber)
+        return randomNumber
+    }
+}
---
+++ ch2_numberGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ch2_numberGame
+//
+//  Created by 한철희 on 3/13/24.
+//
+
+let game = BaseballGame()
+game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+
+
+
