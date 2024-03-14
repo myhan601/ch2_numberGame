495bbc4acd3ec81b27e1a3059604e4f3a232e27b
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-03-14 20:17:32 +0900

    lv.4 clear

--- ch2_numberGame/main.swift
+++ ch2_numberGame/main.swift
@@ -6,8 +6,18 @@
 //
 
 //let game = BaseballGame1() // lv.1 실행
-let game = BaseballGame2() // lv.2 실행
-game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+let game = BaseballGame2() // lv.2, lv.3 실행
+
+// Lv.4 구현
+print("환영합니다! 원하시는 번호를 입력해주세요")
+print("1. 게임 시작하기  2. 게임 기록 보기   3. 종료하기")
+let menuInput = Int(readLine()!)
+
+if menuInput == 1 {
+    print("< 게임을 시작합니다 >")
+    game.start()
+}
+ // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
 
 
 
